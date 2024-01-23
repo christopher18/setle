22411243dfecb42461a668b0bfea6274e9e9da82
Chris Acker <christopheracker1@gmail.com>
2024-01-22 23:52:06 -0800

    Sidebar version 1

--- src/components/navmenu/navmenu.js
+++ src/components/navmenu/navmenu.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { useState } from 'react';
+// import MenuIcon from '@mui/icons-material/Menu';
+// import IconButton from '@mui/material/IconButton';
+// npm install @mui/material @emotion/react @emotion/styled
 import { Link } from 'react-router-dom';
 import './navmenu.css'; // Importing the CSS file
 
 import icon from '../../assets/images/icon_transparent.ico'; // Importing the image file
 
 function NavigationMenu() {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const togglePanel = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <AppBar position="static" className="appBar">
             <Toolbar className="appBar">
@@ -16,13 +27,48 @@ function NavigationMenu() {
                 {/* <Button color="inherit" component={Link} to="/" className="navButton">
                     Home
                 </Button> */}
-                <Button color="inherit" component={Link} to="/puzzle" className="navButton">
+                {/* <Button color="inherit" component={Link} to="/puzzle" className="navButton">
                     Puzzle
                 </Button>
                 <Button color="inherit" component={Link} to="/game" className="navButton">
                     Game
-                </Button>
+                </Button> */}
                 {/* Add more buttons for additional links/screens */}
+                {/* hamburger menu button */}
+                <button className={`hamburger ${isOpen ? 'is-active' : ''}`} onClick={togglePanel}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
+                <div style={{
+                    position: 'fixed',
+                    top: '0',
+                    right: '0',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    backgroundColor: '#4A148C',
+                    boxShadow: '0 0 10px black',
+                    overflowX: 'hidden',
+                    width: isOpen ? '250px' : '0',
+                    height: '100%',
+                    transition: '0.3s',
+                    paddingTop: '60px',
+                    paddingLeft: isOpen ? '10px' : '0',
+                    paddingRight: isOpen ? '10px' : '0',
+                    // Add more styling as needed
+                }} 
+                // className={`slide-out-menu ${isOpen ? 'is-active' : ''}`}
+                >
+                    <Button color="inherit" component={Link} to="/" className="navButton" onClick={togglePanel}>
+                        Home
+                    </Button>
+                    <Button color="inherit" component={Link} to="/puzzle" className="navButton" onClick={togglePanel}>
+                        Puzzle
+                    </Button>
+                    <Button color="inherit" component={Link} to="/game" className="navButton" onClick={togglePanel}>
+                        Game
+                    </Button>
+                </div>
             </Toolbar>
         </AppBar>
     );
